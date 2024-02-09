e2ed7358877824139af9c9dfc7e8d0ecc4021e63
ALFiX <boyarov.danil71@mail.ru>
2024-02-09 20:20:41 +0300

    upd3

--- Files/DisDefender.bat
+++ Files/DisDefender.bat
@@ -1,4 +1,9 @@
 @echo off
 taskkill /f /im smartscreen.exe
+if exist "%SystemRoot%\System32\smartscreen.exe" (
 del /f /q "%SystemRoot%\System32\smartscreen.exe" >nul 2>&1
+)
+
+if exist "%SystemRoot%\System32\smartscreen.dll" (
 del /f /q "%SystemRoot%\System32\smartscreen.dll" >nul 2>&1
+)
