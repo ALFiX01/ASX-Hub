959dad1af269c5b1f59a21f88d2e8f7bef906295
ALFiX <boyarov.danil71@mail.ru>
2025-05-10 13:55:52 +0300

    fix

--- docs/script.js
+++ docs/script.js
@@ -179,7 +179,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // 5. Инициализация Vanilla Tilt JS
     if (typeof VanillaTilt !== 'undefined') {
         VanillaTilt.init(document.querySelectorAll("[data-tilt]"), {
-            reverse: false,
+            reverse: true,
             max: 14,
             speed: 400,
             glare: true,
